cbd2c13c5e1b470d32b67d341aa221cfaf810552
ajdouglass <ajdouglass17@gmail.com>
2016-01-21 18:22:22 -0600

    Update isInteger.cpp

--- Useful-Functions/isInteger.cpp
+++ Useful-Functions/isInteger.cpp
@@ -1,4 +1,4 @@
-bool isInteger(const char* characters) {
+bool stringToInteger(const char* characters) {
     for(int i = 0; characters[i] != 0; i++) {
         if(characters[i] < 48 || characters[i] > 57)
             return false;
