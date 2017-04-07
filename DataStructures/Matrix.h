4745bdc24a36e493407e6eccf3055a8e00c263a2
ajdouglass <ajdouglass17@gmail.com>
2017-04-07 00:26:39 -0500

    Update Matrix.h

--- DataStructures/Matrix.h
+++ DataStructures/Matrix.h
@@ -7,185 +7,232 @@
 template <class T>
 class Matrix {
 public:
-    Matrix(int r, int c);
-    ~Matrix();
-    Matrix(const Matrix<T>& mat);
-    Matrix<T>& operator=(const Matrix<T>& mat); 
+  Matrix();
+  Matrix(int rowSize, int colSize);
+  ~Matrix();
+  Matrix(const Matrix<T>& mat);
+  Matrix<T>& operator=(const Matrix<T>& mat); 
 
-    T* operator[](int r) const
-        { return matrix[r]; }
-    T* operator[](int r)
-        { return matrix[r]; }
-    int rows() const
-        { return ROWS; }
-    int columns() const 
-        { return COLS; }
-    T& at(int r, int c) const;
-    T& at(int r, int c);
+  T* operator[](int row) const;
+  T* operator[](int row);
+  int rows() const;
+  int columns() const;
+  T& at(int row, int col) const;
+  T& at(int row, int col);
 
-    bool contains(T item) const;
-    void resize(int r, int c);
-    void clear();
+  bool contains(T item) const;
+  void resize(int rowSize, int colSize);
+  void clear();
 private:
-    int ROWS, COLS;
-    T** matrix;
+  int numRows, numCols;
+  T** elems;
 
-    void deleteMatrix();
+  void deleteMatrix();
 };
 
 template <class T>
-void Matrix<T>::deleteMatrix() {
-    for(int i = 0; i < ROWS; i++)
-        delete[] matrix[i];
-    delete[] matrix;
+Matrix<T>::Matrix() {
+  numRows = 0;
+  numCols = 0;
+  elems = NULL;
 }
 
 template <class T>
-Matrix<T>::Matrix(int r, int c) 
-    :ROWS(r), COLS(c) 
-{
-    if(ROWS < 0 || COLS < 0)
-        throw std::range_error("Arguments must be positive");
-    matrix = new T*[ROWS];
-    for(int i = 0; i < ROWS; i++)
-        matrix[i] = new T[COLS];
-    for(int r = 0; r < ROWS; r++) {
-        for(int c = 0; c < COLS; c++) {
-            matrix[r][c] = T();
-        }
+Matrix<T>::Matrix(int rowSize, int colSize) {
+  if(rowSize < 0 || colSize < 0) {
+    throw std::range_error("Arguments must be positive");
+  }
+  numRows = rowSize;
+  numCols = colSize;
+  elems = new T*[numRows];
+  for(int i = 0; i < numRows; i++) {
+    elems[i] = new T[numCols];
+  }
+  for(int row = 0; row < numRows; row++) {
+    for(int col = 0; col < numCols; col++) {
+      elems[row][col] = T(); 
     }
+  }
 }
 
 template <class T>
 Matrix<T>::~Matrix() {
-    deleteMatrix();
+  deleteMatrix();
 }
 
 template <class T>
-Matrix<T>::Matrix(const Matrix<T>& mat) 
-    :ROWS(mat.ROWS), COLS(mat.COLS), matrix(new T*[ROWS])
-{
-    for(int i = 0; i < ROWS; i++)
-        matrix[i] = new T[COLS];
-    for(int r = 0; r < ROWS; r++) {
-        for(int c = 0; c < COLS; c++) {
-            matrix[r][c] = mat.matrix[r][c];
-        }
+Matrix<T>::Matrix(const Matrix<T>& mat) {
+  numRows = mat.numRows;
+  numCols = mat.numCols; 
+  elems = new T*[numRows];
+  for(int i = 0; i < numRows; i++) {
+    elems[i] = new T[numCols];
+  }
+  for(int row = 0; row < numRows; row++) {
+    for(int col = 0; col < numCols; col++) {
+      elems[row][col] = mat.elems[row][col];
     }
+  }
 }
 
 template <class T>
 Matrix<T>& Matrix<T>::operator=(const Matrix<T>& mat) {
-    if(this == &mat)
-        return *this;
-    if(ROWS == mat.ROWS && COLS == mat.COLS) {
-        for(int r = 0; r < ROWS; r++) {
-            for(int c = 0; c < COLS; c++) {
-                 matrix[r][c] = mat.matrix[r][c];
-            }
-        }
-        return *this;
-    }
-    deleteMatrix();
-    ROWS = mat.ROWS;
-    COLS = mat.COLS;
-    matrix = new T*[ROWS];
-    for(int i = 0; i < ROWS; i++)
-        matrix[i] = new T[ROWS];
-    for(int r = 0; r < ROWS; r++) {
-        for(int c = 0; c < COLS; c++) {
-            matrix[r][c] = mat.matrix[r][c];
-        }
+  if(this == &mat) {
+    return *this;
+  }
+  if(numRows == mat.numRows && numCols == mat.numCols) {
+    for(int row = 0; row < numRows; row++) {
+      for(int col = 0; col < numCols; col++) {
+        elems[row][col] = mat.elems[row][col];
+      }
     }
     return *this;
+  }
+  deleteMatrix(); 
+  numRows = mat.numRows;
+  numCols = mat.numCols; 
+  elems = new T*[numRows];
+  for(int i = 0; i < numRows; i++) {
+    elems[i] = new T[numRows];
+  }
+  for(int row = 0; row < numRows; row++) {
+    for(int col = 0; col < numCols; col++) {
+      elems[row][col] = mat.elems[row][col];
+    }
+  }
+  return *this;
 }
 
 template <class T>
-T& Matrix<T>::at(int r, int c) const {
-    if(r < 0 || r >= ROWS)
-        throw std::range_error("Rows out of range");
-    if(c < 0 || c >= COLS) 
-        throw std::range_error("Columns out of range");
-    return matrix[r][c];
+T* Matrix<T>::operator[](int row) const {
+  return elems[row];
 }
 
 template <class T>
-T& Matrix<T>::at(int r, int c) {
-    if(r < 0 || r >= ROWS)
-        throw std::range_error("Rows out of range");
-    if(c < 0 || c >= COLS)
-        throw std::range_error("Columns out of range");
-    return matrix[r][c];
+T* Matrix<T>::operator[](int row) {
+  return elems[row];
+}
+
+template <class T>
+int Matrix<T>::rows() const {
+  return numRows;
+}
+
+template <class T>
+int Matrix<T>::columns() const {
+  return numCols;
+}
+
+template <class T>
+T& Matrix<T>::at(int row, int col) const {
+  if(row < 0 || row >= numRows) {
+    throw std::range_error("Row index out of range");
+  }
+  if(col < 0 || col >= numCols) {
+    throw std::range_error("Col index out of range");
+  }
+  return elems[row][col];
+}
+
+template <class T>
+T& Matrix<T>::at(int row, int col) {
+  if(row < 0 || row >= numRows) {
+    throw std::range_error("Row index out of range");
+  }
+  if(col < 0 || col >= numCols) {
+    throw std::range_error("Column index out of range");
+  }
+  return elems[row][col];
 }
 
 template <class T>
 bool Matrix<T>::contains(T item) const {
-    for(int i = 0; i < ROWS; i++) {
-        for(int j = 0; j < COLS; j++) {
-            if(matrix[i][j] == item) 
-                return true;
-        }
+  for(int row = 0; row < numRows; row++) {
+    for(int col = 0; col < numCols; col++) {
+      if(elems[row][col] == item) {
+        return true;
+      }
     }
-    return false;
+  }
+  return false;
 }
 
 template <class T>
-void Matrix<T>::resize(int r, int c) {
-    if(r < 0 || c < 0)
-        throw std::range_error("Arguments must be positive");
-    if(r < ROWS || c < COLS)
-        throw std::invalid_argument("Can't resize to smaller dimensions");
-    T** p = new T*[r];
-    for(int i = 0; i < r; i++)
-        p[i] = new T[c];
-    for(int i = 0; i < r; i++) {
-        for(int j = 0; j < c; j++) {
-             if(i >= ROWS || j >= COLS)  p[i][j] = T();
-             else  p[i][j] = matrix[i][j];
-        }
+void Matrix<T>::resize(int rowSize, int colSize) {
+  if(rowSize < 0 || colSize < 0) {
+    throw std::range_error("Arguments must be positive");
+  }
+  T** newMat = new T*[rowSize];
+  for(int i = 0; i < rowSize; i++) {
+    newMat[i] = new T[colSize];
+  }
+  for(int row = 0; row < rowSize; row++) {
+    for(int col = 0; col < colSize; col++) {
+      if(row >= numRows || col >= numCols)  {
+        newMat[row][col] = T();
+      }
+      else {
+        newMat[row][col] = elems[row][col];
+      }
     }
-    deleteMatrix();
-    matrix = p;
-    ROWS = r;
-    COLS = c;
+  }
+  deleteMatrix();
+  elems = newMat;
+  numRows = rowSize;
+  numCols = colSize;
 }
 
 template <class T>
 void Matrix<T>::clear() {
-	for(int r = 0; r < ROWS; r++) {
-		for(int c = 0; c < COLS; c++) {
-			matrix[r][c] = T();
-		}
-	}
+  for(int row = 0; row < numRows; row++) {
+    for(int col = 0; col < numCols; col++) {
+      elems[row][col] = T();
+    }
+  }
 }
 
 template <class T>
 bool operator==(const Matrix<T>& m1, const Matrix<T>& m2) {
-    if((m1.rows() != m2.rows()) || (m1.columns() != m2.columns()))
+  if((m1.rows() != m2.rows()) || (m1.columns() != m2.columns())) {
+    return false;
+  }
+  for(int row = 0; row < m1.rows(); row++) {
+    for(int col = 0; col < m2.columns(); col++) {
+      if(m1[row][col] != m2[row][col]) {
         return false;
-    for(int r = 0; r < m1.rows(); r++) {
-        for(int c = 0; c < m2.columns(); c++) {
-            if(m1[r][c] != m2[r][c])
-                return false;
-        }
+      }
     }
-    return true;
+  }
+  return true;
+}
+
+template <class T>
+void Matrix<T>::deleteMatrix() {
+  for(int i = 0; i < numRows; i++) {
+    delete[] elems[i];
+  }
+  delete[] elems;
 }
 
 template <class T>
 std::ostream& operator<<(std::ostream& out, const Matrix<T>& mat) {
+  out << '[';
+  for(int row = 0; row < mat.rows(); row++) {
     out << '[';
-    for(int i = 0; i < mat.rows(); i++) {
-        out << '[';
-        for(int j = 0; j < mat.columns(); j++) {
-             out << mat[i][j];
-             if(j != mat.columns()-1) 
-                 out << ", "; 
-        }
-        if(i == mat.rows()-1)  out << "]]";
-        else  out << "]\n ";
+    for(int col = 0; col < mat.columns(); col++) {
+      out << mat[row][col];
+      if(col != mat.columns()-1) {
+        out << ", "; 
+      }
     }
-    return out << ']';
+    if(row == mat.rows()-1) {
+      out << "]]";
+    }
+    else {
+      out << "]\n ";
+    }
+  }
+  return out << ']';
 }
 
 #endif
